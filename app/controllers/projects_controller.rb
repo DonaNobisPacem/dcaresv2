1f36a317504a0b04165926be2ffd53b975fd9fe6
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-05-29 16:51:04 +0900

    Added Delete Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,9 +70,10 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
+    @redirect_url = University.find( @project.university_id )
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to @redirect_url, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
