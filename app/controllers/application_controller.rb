1c0b5fc0e3adea717cc22c0ad129a6f55bd17555
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-06-02 17:02:38 +0900

    Tweaked views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  	protect_from_forgery with: :exception
-  	before_filter :set_university_list
+  protect_from_forgery with: :exception
+  before_filter :set_university_list
 	before_action :configure_permitted_parameters, if: :devise_controller?
   	
   	def set_university_list
