9d54f1ed3263bfe9a9c75eb0beb9cc5a95ccd458
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-06-17 11:08:56 +0900

    Fixed nonworking approved tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :can_edits
   has_many :universities, through: :can_edits
+
+  def active_for_authentication? 
+    super && approved? 
+  end 
+
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
 end
