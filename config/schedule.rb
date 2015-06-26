d0f9db45de1a909739578e72e0e9b1e1be1a4a16
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-06-26 16:55:33 +0900

    reanabled tests

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,13 +22,13 @@ env :PATH, ENV['PATH']
 
 set :output, {:error => "#{path}/log/cron_error_log.log", :standard => "#{path}/log/cron_log.log"}
 
-# every 1.minute do
-#     command "date"
-# end
-# 
-# every 5.minutes do
-# 	rake "notifications:test_send_email"
-# end
+every 1.minute do
+    command "date"
+end
+
+every 5.minutes do
+	rake "notifications:test_send_email"
+end
 
 every :monday, :at => "6am" do
 	rake "notifications:send_email"
