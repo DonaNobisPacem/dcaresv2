48e8438bfab5b1e5fb878a2ce44313767a782fbc
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-07-22 11:33:21 +0900

    Added missing variation percentage function to project components and phases

--- app/models/project_phase.rb
+++ app/models/project_phase.rb
@@ -6,4 +6,16 @@ class ProjectPhase < ActiveRecord::Base
   	has_many :fund_sources, through: :fundable_phases
 
 	accepts_nested_attributes_for :fund_sources
+
+	def variation_percentage
+		if financial_variation.present?
+			if financial_contract_price.present?
+				return 100*financial_variation/financial_contract_price
+			else
+				if financial_budget.present?
+					return 100*financial_variation/financial_budget
+				end
+			end
+		end
+	end
 end
