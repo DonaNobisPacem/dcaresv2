1490c43e963ba6cdaed7402969e061dbc9f40d91
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-08-04 13:35:05 +0900

    fixed table dependency for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :can_edits
+  has_many :can_edits, :dependent => :destroy
   has_many :universities, through: :can_edits
 
   def active_for_authentication? 
