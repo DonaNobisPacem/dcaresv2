0c9ec44c5de8c7de4a1e6ae70be7aa89080f8d9e
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-08-28 18:35:36 +0900

    updated analytics page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -29,6 +29,20 @@ class StaticPagesController < ApplicationController
 		@q = Project.ransack(params[:q])
 		@output = @q.result
 		@projects = @output.paginate(:page => params[:page], :per_page => 10)
+
+		@group_by_cu = @output.group_by { |x| x[:university_id] }
+		@group_by_status = @output.group_by { |x| x[:status] }
+		@group_by_pc = @output.group_by { |x| x[:classification] }
+		@budget_distribution_per_cu = @output.group(:university_id).order('university_id asc').sum(:financial_budget)
+
+		@status_vs_src_vs_classification = []
+		ProjectStatus.find_each do |status|
+			@status_vs_src_vs_classification << {:name => status.description, :data => @output.where( status: status.id ).joins(:fund_sources).group("fund_sources.fund_source_classification").order('fund_sources.fund_source_classification asc').sum(:financial_budget)}
+		end
+		@status_vs_src_vs_classification_count = []
+		ProjectStatus.find_each do |status|
+			@status_vs_src_vs_classification_count << {:name => status.description, :data => @output.where( status: status.id ).joins(:fund_sources).group("fund_sources.fund_source_classification").order('fund_sources.fund_source_classification asc').count}
+		end
 	end
 
 	def search_users
